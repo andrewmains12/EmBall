69a392a7893d451fd3a25dbf917708024538ce67
Andrew Mains <andrewmains12@berkeley.edu>
2012-01-13 00:08:10 -0800

    Refactored test_event_handlers slightly

--- emball/test/test_event_handlers.py
+++ emball/test/test_event_handlers.py
@@ -11,11 +11,7 @@ class MockGameWindow (GameWindow):
         #Initialize self.event_handlers and nothing else, since we don't need 
         #screens and such
         self.event_handlers = {}
-        
-        
-    def on_mousebuttondown(self, event, **kwargs):
-        pass
-
+            
 class EventHandlerTest(unittest.TestCase):
     
     def setUp (self):
@@ -32,7 +28,11 @@ class EventHandlerTest(unittest.TestCase):
         self.assertEqual(self.game_window.event_handlers[QUIT], handler)
         
     def test_auto_discover_handlers (self):
-        """Test that on_mousebuttondown was successfully discovered and registered"""
+        """Test that on_mousebuttondown was successfully discovered and registered""" 
+        def on_mousebuttondown(self, event, **kwargs):
+            pass
+        
+        self.game_window.on_mousebuttondown = on_mousebuttondown
         self.game_window.auto_discover_handlers()
         self.assertEqual(self.game_window.event_handlers[MOUSEBUTTONDOWN],
                          self.game_window.on_mousebuttondown)
